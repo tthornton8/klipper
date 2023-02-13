cf31d591a16aa9cc794de08ae1a78f6e03c93372
tthornton8 <timothythornton8@gmail.com>
2023-02-13 22:27:21 +0000

    bugs

--- klippy/extras/sonic_bed_level.py
+++ klippy/extras/sonic_bed_level.py
@@ -18,6 +18,9 @@ class SonicBedLevel:
 		self.gcode.register_command("VIBRATE_EXTRUDER",
 					self.cmd_VIBRATE,
 					desc=self.cmd_VIBRATE_help)
+		self.gcode.register_command("VIBRATE_EXTRUDER_NEW",
+					self.cmd_VIBRATE_new,
+					desc=self.cmd_VIBRATE_help)
 		self.gcode.register_command("STOP_VIBRATE_EXTRUDER",
 					self.cmd_STOP_VIBRATE,
 					desc=self.cmd_STOP_VIBRATE_help)
@@ -29,7 +32,39 @@ class SonicBedLevel:
 		mcu = pin_params['chip']
 		self.mcu_endstop = mcu.setup_pin('endstop', pin_params)
 
-	def cmd_VIBRATE(self, gcmd, all_endstop_trigger=None):
+	def cmd_VIBRATE(self, gcmd):
+		toolhead = self.printer.lookup_object('toolhead')
+
+		systime = self.printer.get_reactor().monotonic()
+		toolhead_info = toolhead.get_status(systime)
+		old_max_accel = toolhead_info['max_accel']
+		old_max_accel_to_decel = toolhead_info['max_accel_to_decel']
+		max_accel = self.freq * self.accel_per_hz
+		self.gcode.run_script_from_command(
+                "SET_VELOCITY_LIMIT ACCEL=%.3f ACCEL_TO_DECEL=%.3f" % (
+                    max_accel, max_accel))
+
+		self.vibr = True
+		t = time.time()
+		while self.vibr:
+			X, Y, Z, E = toolhead.get_position()
+
+			t_seg = .25 / self.freq
+			accel = self.accel_per_hz * self.freq
+			max_v = accel * t_seg
+			toolhead.move([X, Y, Z, E + self.incr], max_v)
+			toolhead.move([X, Y, Z, E], max_v)
+
+			self.incr *= -1
+
+			if time.time() - t > self.dt:
+				self.vibr = False
+
+		self.gcode.run_script_from_command(
+                "SET_VELOCITY_LIMIT ACCEL=%.3f ACCEL_TO_DECEL=%.3f" % (
+                    old_max_accel, old_max_accel_to_decel))
+
+	def cmd_VIBRATE_new(self, gcmd, all_endstop_trigger=None):
 		self.toolhead = self.printer.lookup_object('toolhead')
 
 		systime = self.printer.get_reactor().monotonic()
